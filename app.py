98b322c6abff117cafd6fbacc6a398e34fd3a6c0
tb9757 <tb9757@gmail.com>
2026-03-26 16:29:44 +0000

    Add code to load data into a dataframe

--- app.py
+++ app.py
@@ -1,6 +1,14 @@
+import os
 import pandas as pd
 import streamlit as st
 
 st.set_page_config(page_title="School of Dandori", page_icon="🌘")
 st.title("🌘 School of Dandori — Course Finder")
 st.write("Find your next whimsical adventure")
+
+def load_data():
+    if not os.path.exists("XXX.csv"):
+        return pd.DataFrame()
+    return pd.read_csv("XXX.csv")
+
+df = load_data()
