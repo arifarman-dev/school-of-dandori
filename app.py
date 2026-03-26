b2c517d288d167dcaf1b57b704f99fa30c772338
tb9757 <tb9757@gmail.com>
2026-03-26 16:27:39 +0000

    Add title headings

---
+++ app.py
@@ -0,0 +1,6 @@
+import pandas as pd
+import streamlit as st
+
+st.set_page_config(page_title="School of Dandori", page_icon="🌘")
+st.title("🌘 School of Dandori — Course Finder")
+st.write("Find your next whimsical adventure")
